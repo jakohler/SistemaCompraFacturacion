136e27c7619e097b91c4d7f4af870c3e9a0ff7d2
jakohler <jaelkohler@gmail.com>
2024-05-24 11:40:12 -0300

    Creando login/logout

--- app/settings.py
+++ app/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  #Le indicamos como se ll
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
