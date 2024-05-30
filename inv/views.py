dc678346a5db385bc49f68d91959a0e74e391fc9
jakohler <jaelkohler@gmail.com>
2024-05-30 11:39:47 -0300

    Creando y agregando Productos

--- inv/views.py
+++ inv/views.py
@@ -4,8 +4,8 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Categoria, SubCategoria, Marca, UnidadMedida
-from .forms import CategoriaForm, SubCategoriaForm, MarcaForm, UMForm
+from .models import Categoria, SubCategoria, Marca, UnidadMedida, Producto
+from .forms import CategoriaForm, SubCategoriaForm, MarcaForm, UMForm, ProductoForm
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
@@ -178,5 +178,70 @@ def um_inactivar(request, id):
         um.estado=False
         um.save()
         return redirect("inv:um_list")
+
+    return render(request,template_name,contexto)
+
+
+class ProductoView(LoginRequiredMixin, generic.ListView):
+    model = Producto
+    template_name = "inv/prducto_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+class ProductoNew(LoginRequiredMixin, generic.CreateView):
+    model=Producto
+    template_name="inv/producto_form.html"
+    context_object_name = 'obj'
+    form_class=ProductoForm
+    success_url= reverse_lazy("inv:producto_list")
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductoNew, self).get_context_data(**kwargs)
+        context["categorias"] = Categoria.objects.all()
+        context["subcategorias"] = SubCategoria.objects.all()
+        return context
+
+class ProductoEdit(LoginRequiredMixin, generic.UpdateView):
+    model=Producto
+    template_name="inv/producto_form.html"
+    context_object_name = 'obj'
+    form_class=ProductoForm
+    success_url= reverse_lazy("inv:producto_list")
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        pk = self.kwargs.get('pk')
+
+        context = super(ProductoEdit, self).get_context_data(**kwargs)
+        context["categorias"] = Categoria.objects.all()
+        context["subcategorias"] = SubCategoria.objects.all()
+        context["obj"] = Producto.objects.filter(pk=pk).first()
+
+        return context
+
+def producto_inactivar(request, id):
+    prod = Producto.objects.filter(pk=id).first()
+    contexto={}
+    template_name="inv/categoria_del.html"
+
+    if not prod:
+        return redirect("inv:producto_list")
+    
+    if request.method=='GET':
+        contexto={'obj':prod}
+    
+    if request.method=='POST':
+        prod.estado=False
+        prod.save()
+        return redirect("inv:producto_list")
 
     return render(request,template_name,contexto)
