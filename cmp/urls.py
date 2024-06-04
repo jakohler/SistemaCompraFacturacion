4ab52e8b71aca12f0ab8e9c667ed6b6ba110707b
jakohler <jaelkohler@gmail.com>
2024-06-04 12:21:02 -0300

    creando y agregando reportes con pdf

--- cmp/urls.py
+++ cmp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import ProveedorView, ProveedorNew, ProveedorEdit, proveedorInactivar,\
                 ComprasView, compras, CompraDetDelete
 
-# from .reportes import reporte_compras, imprimir_compra
+from .reportes import reporte_compras, imprimir_compra
 
 urlpatterns = [
     path('proveedores/',ProveedorView.as_view(), name="proveedor_list"),
@@ -16,6 +16,6 @@ urlpatterns = [
     path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
 
-    # path('compras/listado', reporte_compras, name='compras_print_all'),
-    # path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
+    path('compras/listado', reporte_compras, name='compras_print_all'),
+    path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
 ]
