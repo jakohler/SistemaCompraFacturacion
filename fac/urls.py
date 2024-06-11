43fc1376ee8fa2e6aa65132320b01baf7adbbf69
jakohler <jaelkohler@gmail.com>
2024-06-11 11:58:43 -0300

    Terminando con modulo facturacion

--- fac/urls.py
+++ fac/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
+from fac.reportes import imprimir_factura_list, imprimir_factura_recibo
+
 from .views import ClienteView, ClienteEdit, ClienteNew, clienteInactivar, \
     FacturaView, facturas, \
-    ProductoView
+    ProductoView, \
+    borrar_detalle_factura
 
 # from .reportes import imprimir_factura_recibo, imprimir_factura_list
 
@@ -18,11 +21,11 @@ urlpatterns = [
     
     path('facturas/buscar-producto',ProductoView.as_view(), name="factura_producto"),
 
-    # path('facturas/borrar-detalle/<int:id>',borrar_detalle_factura, name="factura_borrar_detalle"),
+    path('facturas/borrar-detalle/<int:id>',borrar_detalle_factura, name="factura_borrar_detalle"),
 
-    # path('facturas/imprimir/<int:id>',imprimir_factura_recibo, name="factura_imprimir_one"),
+    path('facturas/imprimir/<int:id>',imprimir_factura_recibo, name="factura_imprimir_one"),
 
-    # path('facturas/imprimir-todas/<str:f1>/<str:f2>',imprimir_factura_list, name="factura_imprimir_all"),
+    path('facturas/imprimir-todas/<str:f1>/<str:f2>',imprimir_factura_list, name="factura_imprimir_all"),
 
     # path('facturas/clientes/new/',cliente_add_modify,name="fac_cliente_add"),
     # path('facturas/clientes/<int:pk>',cliente_add_modify,name="fac_cliente_mod"),
